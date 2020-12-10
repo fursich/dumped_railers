d160f074d02f6c8148a4cc31ed5564db0ebc67da
Koji Onishi <fursich0@gmail.com>
2020-12-11 02:20:01 +0900

    make sure that the test runs with clean state

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,40 @@
-require "bundler/setup"
+require 'bundler/setup'
+require 'pry'
+require 'database_cleaner/active_record'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), 'support'))
 require 'active_record_helper'
+require 'active_record_models'
 
 require 'dumped_railers'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  # need to maintain class-baseed caches in clean state
+  config.around(:each) do  |example|
+    example.run
+    DumpedRailers::RecordBuilder::FixtureRow::RecordStore.clear!
+    DumpedRailers::RecordBuilder::DependencyTracker.clear!
   end
 end
--- spec/support/active_record_helper.rb
+++ spec/support/active_record_helper.rb
@@ -1,7 +1,11 @@
 require 'active_record'
 require 'yaml'
 
+File.delete('spec/support/dumped_railers.sqlite3') if File.exist? 'spec/support/dumped_railers.sqlite3'
+
 dbconfig = YAML::load(File.read(File.join(File.dirname(__FILE__), 'database.yml')))
-ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
+ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '../tmp/debug.log'))
+
+ActiveRecord::Base.belongs_to_required_by_default = true
 ActiveRecord::Base.establish_connection(dbconfig[ENV['DB'] || 'sqlite'])
 
