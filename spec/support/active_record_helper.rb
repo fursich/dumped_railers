ea71dd0c68dfb73730ec251a2ec8f0d0c11dc29c
Koji Onishi <fursich0@gmail.com>
2020-12-11 05:28:12 +0900

    change path for ActiveRecord Logger in test env

--- spec/support/active_record_helper.rb
+++ spec/support/active_record_helper.rb
@@ -4,7 +4,7 @@ require 'yaml'
 File.delete('spec/support/dumped_railers.sqlite3') if File.exist? 'spec/support/dumped_railers.sqlite3'
 
 dbconfig = YAML::load(File.read(File.join(File.dirname(__FILE__), 'database.yml')))
-ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), '../tmp/debug.log'))
+ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), 'debug.log'))
 
 ActiveRecord::Base.belongs_to_required_by_default = true
 ActiveRecord::Base.establish_connection(dbconfig[ENV['DB'] || 'sqlite'])
