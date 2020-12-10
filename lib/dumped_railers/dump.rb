e4f3f1559300a1c47275e03a3d278830bcf9200c
Koji Onishi <fursich0@gmail.com>
2020-12-11 02:34:46 +0900

    implement dump handler

---
+++ lib/dumped_railers/dump.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'fixture_builder/model'
+require_relative 'preprocessor/strip_ignorables'
+
+module DumpedRailers
+  class Dump
+    def initialize(*models, preprocessors: [])
+      @fixture_tables = models.map { |model|
+        FixtureBuilder::Model.new(model, preprocessors: preprocessors)
+      }
+    end
+
+    def build_fixtures!
+      @fixtures = @fixture_tables.map(&:build!).to_h
+    end
+
+    def persist_all!(base_dir)
+      FileUtils.mkdir_p(base_dir)
+      FileHelper.write(*@fixtures, base_dir: base_dir)
+    end
+  end
+end
---
+++ spec/lib/dumped_railers/dump_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+RSpec.describe DumpedRailers::Dump do
+  describe '#build_fixtures!' do
+    let(:fixture_generator) { described_class.new(*models, preprocessors: preprocessors) }
+
+    subject { fixture_generator.build_fixtures! }
+
+    let!(:author1)  { Author.create!(name: 'William Shakespeare') }
+    let!(:author2)  { Author.create!(name: 'Shikibu Murasaki') }
+    let!(:article1) { Article.create!(title: 'Romeo and Juliet', author: author1) }
+    let!(:article2) { Article.create!(title: 'King Lear',        author: author1) }
+    let!(:article3) { Article.create!(title: 'Genji Monogatari', author: author2) }
+    let(:models)    { [Author, Article] }
+
+    let(:preprocessors) { [] }
+
+    it {
+      is_expected.to match(
+        'authors' =>
+         {
+          '_fixture' =>
+            {
+              'model_class'          => 'Author',
+              'fixture_generated_by' => 'DumpedRailers',
+            },
+          "__author_#{author1.id}" => {
+             'id'   => author1.id,
+             'name' => author1.name
+            },
+          "__author_#{author2.id}" => {
+             'id'   => author2.id,
+             'name' => author2.name
+            },
+        },
+        'articles' =>
+        {
+         '_fixture' =>
+           {
+             'model_class'          => 'Article',
+             'fixture_generated_by' => 'DumpedRailers',
+           },
+         "__article_#{article1.id}" => {
+            'id'     => article1.id,
+            'title'  => article1.title,
+            'author' => "__author_#{author1.id}",
+           },
+         "__article_#{article2.id}" => {
+            'id'     => article2.id,
+            'title'  => article2.title,
+            'author' => "__author_#{author1.id}",
+           },
+         "__article_#{article3.id}" => {
+            'id'     => article3.id,
+            'title'  => article3.title,
+            'author' => "__author_#{author2.id}",
+           },
+        }
+      )
+    }
+  end
+end
